5be99fd6acf79a61ffd61a1fc27a2712a17075b0
ENGAbderahmanZaghloul <114342047+ENGAbderahmanZaghloul@users.noreply.github.com>
2025-02-21 03:46:54 +0200

    add sign up page ui

--- src/components/common/ui/Button.tsx
+++ src/components/common/ui/Button.tsx
@@ -3,6 +3,7 @@ import Button from "@mui/material/Button";
 interface ButtonProps {
   children: React.ReactNode;
   className?: string;
+  type?: string;
 }
 
 export default function CustomButton({ children, className }: ButtonProps) {

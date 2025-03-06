701489a30bf49b5148ecb1e55a5b5ff05c6aafec
ENGAbderahmanZaghloul <114342047+ENGAbderahmanZaghloul@users.noreply.github.com>
2025-03-06 08:52:19 +0200

    wishlist page

--- src/stores/cart/cartActions.ts
+++ src/stores/cart/cartActions.ts
@@ -1,34 +1,3 @@
-// import { CartItem } from "@/types/CartTypes";
-
-// export const addToCart = (cart: CartItem[], product: CartItem) => {
-//   const existingItem = cart.find(
-//     (item) => item.ProductId === product.ProductId
-//   );
-//   if (existingItem) {
-//     return cart.map((item) =>
-//       item.ProductId === product.ProductId
-//         ? { ...item, quantity: (item.quantity ?? 0) + 1 }
-//         : item
-//     );
-//   }
-//   return [...cart, { ...product, quantity: 1 }];
-// };
-
-// export const removeFromCart = (cart: CartItem[], id: number) => {
-//   return cart.filter((item) => item.ProductId !== id);
-// };
-
-// export const updateQuantity = (
-//   cart: CartItem[],
-//   id: number,
-//   quantity: number
-// ) => {
-//   return cart.map((item) =>
-//     item.ProductId === id ? { ...item, quantity } : item
-//   );
-// };
-
-
 import { CartItem } from "@/types/CartTypes";
 
 export const addToCart = (cart: CartItem[], cartItem: CartItem) => {

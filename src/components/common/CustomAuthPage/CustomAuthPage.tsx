97aa27376e8179ea70d0d18e957b1ee50d10ef6b
ENGAbderahmanZaghloul <114342047+ENGAbderahmanZaghloul@users.noreply.github.com>
2025-04-04 19:41:11 +0200

    login & sinup responsive

--- src/components/common/CustomAuthPage/CustomAuthPage.tsx
+++ src/components/common/CustomAuthPage/CustomAuthPage.tsx
@@ -25,33 +25,33 @@ export const CustomAuthPage = ({
 
 }: CustomAuthPageProps) => {
   return (
-    <div className="flex justify-between items-center pt-10 pb-24 pr-24 ">
+    <div className="flex justify-between items-center pt-10 pb-24 pr-24 tab-large-screen:pr-0 ">
       <div className="flex items-center ">
         <Image
           src="/images/login.png"
           alt="login img"
           width={800}
           height={780}
-          className="object-contain"
+          className="object-contain tab-large-screen:w-[570px] custom-mid:w-[410px] custom-handling:hidden"
         />
       </div>
-      <div className="h-[780px] py-24 flex flex-col justify-center gap-4 w-1/3 mx-auto">
+      <div className="h-[780px] py-24 flex flex-col justify-center gap-4 w-1/3 custom-handling:w-[80%] mx-auto">
         <div className="flex flex-col gap-6 mb-3">
-          <h1 className="text-4xl font-medium">{title}</h1>
-          <p className="text-base font-light">Enter your details below</p>
+          <h1 className="text-4xl font-medium custom-sm:text-2xl custom-phone:text-base">{title}</h1>
+          <p className="text-base font-light custom-sm:text-sm custom-sm:font-thin custom-phone:text-xs">Enter your details below</p>
         </div>
         <div>
           <CustomForm fieldData={form} initialValues={initialValues} isLogin={isLogin} />
         </div>
         {isLogin ? (
-          <div className="flex gap-2 items-center justify-center mt-3">
+          <div className="flex gap-2 items-center justify-center mt-3 custom-phone:!flex-col custom-phone:text-center">
             <p className="text-[14px] "> Don&apos;t Have an Account ? </p>
             <Link className="text-[14px] border-b border-black " href="/SignUp">
               SignUp
             </Link>
           </div>
         ) : (
-          <div className="flex gap-2 items-center justify-center mt-3">
+          <div className="flex gap-2 items-center justify-center mt-3 custom-phone:!flex-col custom-phone:text-center">
             <p className="text-[14px] ">Already have an account ? </p>
             <Link className="text-[14px] border-b border-black " href="/login">
               Login

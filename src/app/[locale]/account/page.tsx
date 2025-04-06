c285832a37931eee1f550bfb280e4519d6878ee2
ENGAbderahmanZaghloul <114342047+ENGAbderahmanZaghloul@users.noreply.github.com>
2025-04-06 06:21:06 +0200

    account page responsive

--- src/app/[locale]/account/page.tsx
+++ src/app/[locale]/account/page.tsx
@@ -30,13 +30,13 @@ const Accountpage = () => {
           Welcome! <span className="text-red-600">ya 3lk</span>
         </h5>
       </div>
-      <div className="my-12 flex gap-10">
+      <div className="my-12 flex gap-10 custom-handling:flex-col">
         <div className="sidebar flex flex-col gap-4">
           {uls.map((i, index) => (
             <SideBarUl key={index} title={i.title} theLink={i.lis} />
           ))}
         </div>
-        <div className="w-[60%] mx-auto">
+        <div className="w-[60%] mx-auto custom-handling:!w-[100%]">
           <AccountBody />
         </div>
       </div>

043c470985f565d82994e697fe881a0a685cd697
ENGAbderahmanZaghloul <114342047+ENGAbderahmanZaghloul@users.noreply.github.com>
2025-04-06 22:41:51 +0200

    cart responsive

--- src/app/[locale]/(shop)/cart/CartRow.tsx
+++ src/app/[locale]/(shop)/cart/CartRow.tsx
@@ -1,19 +1,18 @@
-"use client"
-import {IoIosArrowUp, IoIosArrowDown} from "react-icons/io";
+"use client";
+import { IoIosArrowUp, IoIosArrowDown } from "react-icons/io";
 import Image from "next/image";
 import IconButton from "@mui/material/IconButton";
 import DeleteIcon from "@mui/icons-material/Delete";
-import {useCartStore} from "@/stores/cart/cartStore";
+import { useCartStore } from "@/stores/cart/cartStore";
 import styles from "@/styles/cart/Cart.module.scss";
-import {CartItem} from "@/types/CartTypes";
-import {useMemo} from "react";
+import { CartItem } from "@/types/CartTypes";
+import { useMemo } from "react";
 
 type CartRowProps = {
   cartItems: CartItem[];
 };
 
-const CartRow = ({cartItems}: CartRowProps) => {
-
+const CartRow = ({ cartItems }: CartRowProps) => {
   const store = useCartStore();
   const removeFromCart = useMemo(() => store.removeFromCart, [store]);
   const updateQuantity = useMemo(() => store.updateQuantity, [store]);
@@ -23,9 +22,9 @@ const CartRow = ({cartItems}: CartRowProps) => {
         <tr key={index} className={styles.tableRow}>
           <td>
             <Image
-              className="flex justify-center items-center mx-auto m-3"
+              className="flex justify-center items-center mx-auto m-3 custom-sm:w-[50px]"
               src={item.product.mainImageUrl}
-              alt={item.product.name || 'product'}
+              alt={item.product.name || "product"}
               width={70}
               height={70}
             />
@@ -33,30 +32,46 @@ const CartRow = ({cartItems}: CartRowProps) => {
           <td className={styles.columns}>{item.product.name}</td>
           <td className={styles.columns}>${item.product.price}</td>
           <td className={styles.columns}>
-            <div
-              className="flex gap-x-2 justify-between items-center mt-1 px-3 py-4 text-base border w-[60px] h-11 border-gray-800 focus:border-black sm:text-sm rounded-md">
+            <div className="flex gap-x-2 justify-between items-center mt-1 px-3 py-4 text-base border w-[60px] h-11 border-gray-800 focus:border-black sm:text-sm rounded-md">
               <p>{item.quantity}</p>
               <div className="flex flex-col justify-center gap-1">
-                <button onClick={() => updateQuantity(item.product.productId, (item.quantity ?? 1) + 1)}>
-                  <IoIosArrowUp/>
+                <button
+                  onClick={() =>
+                    updateQuantity(
+                      item.product.productId,
+                      (item.quantity ?? 1) + 1
+                    )
+                  }
+                >
+                  <IoIosArrowUp />
                 </button>
                 <button
                   onClick={() =>
                     item.quantity && item.quantity > 1
-                      ? updateQuantity(item.product.productId, item.quantity - 1)
+                      ? updateQuantity(
+                          item.product.productId,
+                          item.quantity - 1
+                        )
                       : null
                   }
                 >
-                  <IoIosArrowDown/>
+                  <IoIosArrowDown />
                 </button>
               </div>
             </div>
           </td>
-          <td className={styles.columns}> ${(item.product.price * (item.quantity ?? 1)).toFixed(2)}</td>
           <td className={styles.columns}>
-            <IconButton onClick={() => removeFromCart(item.product.productId)} aria-label="delete" size="large"
-                        color="error">
-              <DeleteIcon fontSize="inherit"/>
+            {" "}
+            ${(item.product.price * (item.quantity ?? 1)).toFixed(2)}
+          </td>
+          <td className={styles.columns}>
+            <IconButton
+              onClick={() => removeFromCart(item.product.productId)}
+              aria-label="delete"
+              size="large"
+              color="error"
+            >
+              <DeleteIcon fontSize="inherit" />
             </IconButton>
           </td>
         </tr>
--- src/app/[locale]/(shop)/cart/page.tsx
+++ src/app/[locale]/(shop)/cart/page.tsx
@@ -5,41 +5,47 @@ import CartHeadRow from "./CartHeadRow";
 import CustomButton from "@/components/common/ui/Button";
 import CartTotal from "./CartTotal";
 import PathLinks from "@/components/common/ui/PathLinks";
-import {useCartStore} from "@/stores/cart/cartStore";
+import { useCartStore } from "@/stores/cart/cartStore";
 import AsyncAddToCart from "@/services/AsyncAddToCart";
 
-
 const CartPage = () => {
-
-  const cartItems = useCartStore((state) => state.cart)
-  const total = cartItems.reduce((acc, cartItem) => acc + (cartItem.subtotal || 0), 0);
+  const cartItems = useCartStore((state) => state.cart);
+  const total = cartItems.reduce(
+    (acc, cartItem) => acc + (cartItem.subtotal || 0),
+    0
+  );
 
   const handleUpdateCart = () => {
     cartItems.map((cartItem) => {
-      AsyncAddToCart({ProductId: cartItem.product.productId, quantity: cartItem.quantity ?? 1});
-    })
-  }
+      AsyncAddToCart({
+        ProductId: cartItem.product.productId,
+        quantity: cartItem.quantity ?? 1,
+      });
+    });
+  };
 
   return (
-    <div className="my-28 mx-auto w-[80%] ">
+    <div className="my-28 mx-auto w-[80%] custom-mid:w-[90%] custom-sm:w-[98%]">
       <div className="flex gap-2 m-0 pb-10 text-slate-400">
-        <PathLinks titles={["Cart"]}/>
+        <PathLinks titles={["Cart"]} />
       </div>
       <div>
         <table className={styles.catTable}>
           <thead className="mb-10">
-          <CartHeadRow/>
+            <CartHeadRow />
           </thead>
           <tbody>
-          <CartRow cartItems={cartItems}/>
+            <CartRow cartItems={cartItems} />
           </tbody>
         </table>
         <div className=" flex justify-between">
           <button className={styles.cartBtn}>Return To Shop</button>
-          <button className={styles.cartBtn} onClick={handleUpdateCart}>Update Cart</button>
+          <button className={styles.cartBtn} onClick={handleUpdateCart}>
+            Update Cart
+          </button>
         </div>
       </div>
-      <div className="mt-16 flex justify-between">
+      <div className="mt-16 flex justify-between tab-large-screen:flex-col tab-large-screen:gap-4 tab-large-screen:place-items-center">
         <div>
           <form className="flex gap-3">
             <input
@@ -48,14 +54,14 @@ const CartPage = () => {
               name="text"
               placeholder="Coupon Code"
             />
-            <CustomButton className="w-[210px] h-[55px]" type="submit">
+            <CustomButton className="w-[210px] h-[55px] custom-sm:w-[120px] custom-sm:text-sm" type="submit">
               Apply Coupon
             </CustomButton>
           </form>
         </div>
-        <div className="border-2 border-black py-5 px-7 w-[470px] text-center  ">
+        <div className="border-2 border-black py-5 px-7 w-[470px] custom-sm:w-[300px] text-center  ">
           <h2 className="my-1 text-start">Cart Total</h2>
-          <CartTotal total={total}/>
+          <CartTotal total={total} />
           <CustomButton className="text-[12px] py-2 px-7 !mt-4 ">
             Process to checkout
           </CustomButton>

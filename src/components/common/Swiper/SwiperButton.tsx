fbc43faca33cf202cd6fc555f1bb6abac20148cf
joo <yo955@gmail.com>
2025-04-30 00:15:01 +0300

    merge abdu to main

--- src/components/common/Swiper/SwiperButton.tsx
+++ src/components/common/Swiper/SwiperButton.tsx
@@ -3,18 +3,22 @@ import CustomButton from "../ui/Button";
 import useGetProducts from "@/hooks/Product/useGetProducts";
 import ProductItem from "@/components/common/products/ProductCard";
 import Loading from "@/loading/Loading";
+import {Link} from "@/i18n/routing";
 
 // import Products from "./products/page";
 
 const SwiperButton = () => {
-  const { data: products, isLoading } = useGetProducts();
-  if (isLoading) return <Loading />;
+  const {data: products, isLoading} = useGetProducts();
+  if (isLoading) return <Loading/>;
   return (
     <div>
-      {products && <CustomSwiper data={products} Card={ProductItem} />}
-      <CustomButton className=" !text-base !my-12 !w-[234px]  !h-[56px]  !border !mx-auto !flex !justify-center !items-center">
-        View All Products
-      </CustomButton>
+      {products && <CustomSwiper data={products} Card={ProductItem}/>}
+      <Link href="/products">
+        <CustomButton
+          className=" !text-base !my-12 !w-[234px]  !h-[56px]  !border !mx-auto !flex !justify-center !items-center">
+          View All Products
+        </CustomButton>
+      </Link>
     </div>
   );
 };
